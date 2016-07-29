23ab3984897605659628b0f4ddb6e8056e05d3f6
Ching <dino8556@gmail.com>
2016-07-29 11:41:55 +0800

    initial commit

--- app/src/main/java/com/example/ching/babysteps/MainActivity.java
+++ app/src/main/java/com/example/ching/babysteps/MainActivity.java
@@ -1,7 +1,13 @@
 package com.example.ching.babysteps;
 
+import android.app.ActionBar;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
