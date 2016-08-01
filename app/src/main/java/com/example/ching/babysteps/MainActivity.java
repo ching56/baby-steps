0bdf42b3c45404e3b6c853f0d55cad2362be3620
Ching <dino8556@gmail.com>
2016-08-01 11:53:11 +0800

    add timeline

--- app/src/main/java/com/example/ching/babysteps/MainActivity.java
+++ app/src/main/java/com/example/ching/babysteps/MainActivity.java
@@ -1,19 +1,86 @@
 package com.example.ching.babysteps;
 
 import android.app.ActionBar;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Build;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 
+import java.sql.Time;
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    // Construct the data source
+    ArrayList<String> arrays;
+    // Create the adapter to convert the array to views
+    TimeLineAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        arrays = new ArrayList<String>();
+        adapter = new TimeLineAdapter(this, arrays);
+        // Attach the adapter to a ListView
+        ListView listView = (ListView) findViewById(R.id.lsview);
+        listView.setAdapter(adapter);
+    }
+    public void addEvent(View view){
+        Button button = (Button) findViewById(R.id.timelineButton);
+        //result = (EditText) findViewById(R.id.editTextResult);
+
+        // get prompts.xml view
+        Context context = this;
+        LayoutInflater li = LayoutInflater.from(context);
+        View promptsView = li.inflate(R.layout.prompt, null);
+
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+                context);
+
+        // set prompts.xml to alertdialog builder
+        alertDialogBuilder.setView(promptsView);
+
+        final EditText userInput = (EditText) promptsView
+                .findViewById(R.id.editTextDialogUserInput);
+        final EditText userInput2 = (EditText) promptsView
+                .findViewById(R.id.editTextDialogUserInput2);
+
+        // set dialog message
+        alertDialogBuilder
+                .setCancelable(false)
+                .setPositiveButton("OK",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog,int id) {
+                                // get user input and set it to result
+                                // edit text
+                                //result.setText(userInput.getText());
+                                adapter.add(userInput.getText()+" "+userInput2.getText());
+
+                            }
+                        })
+                .setNegativeButton("Cancel",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog,int id) {
+                                dialog.cancel();
+                            }
+                        });
+
+        // create alert dialog
+        AlertDialog alertDialog = alertDialogBuilder.create();
+
+        // show it
+        alertDialog.show();
+
     }
 }
