c8d7a510967156177a09989a896804c6b4cae21c
katherine0504 <F74036069@ncku.edu.tw>
2016-08-04 19:18:56 +0800

    Merge all

--- app/src/main/java/com/example/lulu/babystep/PageFragmentSecond.java
+++ app/src/main/java/com/example/lulu/babystep/PageFragmentSecond.java
@@ -1,10 +1,13 @@
 package com.example.lulu.babystep;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * Created by lulu on 2016/8/1.
@@ -35,6 +38,21 @@ public class PageFragmentSecond extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_second, container, false);
+        Button button = (Button) view.findViewById(R.id.shareButton);
+        button.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent = new Intent(Intent.ACTION_SENDTO);
+                intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+                intent.putExtra(Intent.EXTRA_SUBJECT,"Baby data from babySteps");
+                intent.putExtra(Intent.EXTRA_TEXT,"DATA");
+                if (intent.resolveActivity(getActivity().getPackageManager()) != null) {
+                    startActivity(intent);
+                }
+            }
+        });
       //  TextView tvTitle = (TextView) view.findViewById(R.id.tvTitle);
       //  tvTitle.setText("Fragment #" + mPage);
         return view;
